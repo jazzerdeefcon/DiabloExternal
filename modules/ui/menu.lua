a99c8b3504ac83b29d9ce0f74d35e106e137bffc
Defcon <jazzerdefcon@gmail.com>
2025-09-18 02:43:13 -0600

    Agregados init.lua y menu.lua con código base

--- modules/ui/menu.lua
+++ modules/ui/menu.lua
@@ -123,7 +123,7 @@ function menu.init(loadModuleFunc)
     btn.Text = name
     btn.TextColor3 = Color3.fromRGB(255, 255, 255) -- texto blanco
     btn.TextSize = 14
-    btn.Font = Enum.Font.Anton
+    btn.Font = Enum.Font.Arcade
     btn.BackgroundTransparency = 1 -- transparente para mostrar el fondo
     btn.BorderSizePixel = 0
     btn.Parent = parentFrame -- ⚠️ poner como hermano de bg
