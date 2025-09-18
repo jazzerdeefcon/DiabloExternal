ebcc12b5318dda25c425c013dcdd2410911fbe74
Defcon <jazzerdefcon@gmail.com>
2025-09-18 02:50:54 -0600

    Agregados init.lua y menu.lua con código base

--- modules/ui/menu.lua
+++ modules/ui/menu.lua
@@ -124,7 +124,7 @@ function menu.init(loadModuleFunc)
     btn.TextColor3 = Color3.fromRGB(255, 255, 255) -- texto blanco
     btn.TextSize = 14
     --btn.Font = Enum.Font.SciFi
-    btn.Font = Enum.Font.DenkOne
+    btn.Font = Enum.Font.FredokaOne
     btn.BackgroundTransparency = 1 -- transparente para mostrar el fondo
     btn.BorderSizePixel = 0
     btn.Parent = parentFrame -- ⚠️ poner como hermano de bg
