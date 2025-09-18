bb39effa290e58acbbd17aba92481f016eef755d
Defcon <jazzerdefcon@gmail.com>
2025-09-17 21:00:31 -0600

    Agregados init.lua y menu.lua corregidos

--- modules/ui/menu.lua
+++ modules/ui/menu.lua
@@ -24,7 +24,7 @@ function menu.init(core)
     logo.Size = UDim2.new(0, 80, 0, 80)                 -- tamaño del logo
     logo.Position = UDim2.new(0.5, -40, 0, 40)          -- centrado horizontal, 40px desde arriba
     logo.BackgroundTransparency = 1                     -- fondo transparente
-    logo.Image = "rbxassetid://96732684095335"              -- ?? reemplaza con tu assetId del logo
+    logo.Image = "rbxassetid://130020553459788"              -- ?? reemplaza con tu assetId del logo
  
 
     -- Titulo
